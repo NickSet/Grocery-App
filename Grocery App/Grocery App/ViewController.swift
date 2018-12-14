8b09f1716e88de716c2a93090efef367855d71d4
Nick Setliff <nicksetliff@gmail.com>
2018-12-14 12:47:16 -0500

    Initial commit

---
+++ Grocery App/Grocery App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grocery App
+//
+//  Created by Nicholas Setliff on 12/14/18.
+//  Copyright © 2018 Nicholas Setliff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
