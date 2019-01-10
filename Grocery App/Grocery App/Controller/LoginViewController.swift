5b5056b80f5dbc9e32d6901feafd9f56e2453f83
Nick Setliff <nicksetliff@gmail.com>
2019-01-10 11:20:19 -0500

    Add LoginViewController; Setup VC in storyboard

---
+++ Grocery App/Grocery App/Controller/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Grocery App
+//
+//  Created by Nicholas Setliff on 1/10/19.
+//  Copyright © 2019 Nicholas Setliff. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var stayLoggedInSwitch: UISwitch!
+    @IBOutlet var loginButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
